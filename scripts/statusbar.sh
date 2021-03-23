8c6c1ef860761c63db91829192b69a003d9700f8
valdeg <mailme@valdeg.xyz>
2021-03-23 18:50:51 +0200

    Added kblayout indicator

--- scripts/statusbar.sh
+++ scripts/statusbar.sh
@@ -4,8 +4,21 @@ print_date() {
     date "+%H:%M"
 }
 
+print_kb_layout() {
+  layout=$(xset -q | grep LED | awk '{ print $10 }')
+
+  if [[ "$layout" == "00000002" || "$layout" == "00000000" ]]
+  then
+    layout="EN"
+  elif [[ "$layout" == "00000002" || "$layout" == "00001000" ]]; then
+    layout="RU"
+  fi
+
+  echo $layout
+}
+
 while true
 do
-    xsetroot -name "$(print_date)"
-    sleep 10s
+    xsetroot -name "  $(print_kb_layout) | $(print_date) "
+    sleep 0.3s;
 done
